b98c66b66b5c8109d23cc0b7deebe2b08108ed5f
MadCapGipp0n <harski.jatka@yahoo.com>
2023-11-29 21:28:27 +0200

    Portal sammutettu

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class nextscene : MonoBehaviour
-{
-    public string scenename;
+// public class nextscene : MonoBehaviour
+// {
+//     public string scenename;
  
- void OnTriggerEnter(Collider other){
-  if(other.CompareTag("Player")){
-   SceneManager.LoadScene(scenename);
-  }
- }
-}
+//  void OnTriggerEnter(Collider other){
+//   if(other.CompareTag("Player")){
+//    SceneManager.LoadScene(scenename);
+//   }
+//  }
+// }
