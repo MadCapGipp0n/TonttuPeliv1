1dd1e4f7ee8fa0330ca7bd062c2aa38f1325cdc7
MadCapGipp0n <harski.jatka@yahoo.com>
2023-12-10 13:57:33 +0200

    1Kenttä karkki ja playermovement lisätty pohjalle avain

---
+++ Assets/jumpboost.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class jumpboost : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if(other.name == "Player1kent")
+//         {
+//             PlayerMovementV1.jumpForce *=6;
+//             Destroy(gameObject);
+//         }
+//     }
+
+    
+//}
